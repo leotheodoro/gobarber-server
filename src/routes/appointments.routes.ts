82b4a3d943f04b7993eff4b7eddc3bb685b24c71
Leonardo Theodoro <simplespinheiro@gmail.com>
2021-01-13 15:10:25 -0300

    creating appointment  w/ services and SOLID

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
 
 appointmentsRouter.get('/', (request, response) => {
   const appointments = appointmentsRepository.all();
@@ -12,18 +14,19 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+    const parsedDate = parseISO(date);
 
-  if (findAppointmentInSameDate) {
-    return response.status(400).json({ message: 'This apppointment is already booked' });
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+
+    const appointment = createAppointment.execute({ provider, date: parsedDate })
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create({ provider, date: parsedDate });
-
-  return response.json(appointment);
 
 });
 
