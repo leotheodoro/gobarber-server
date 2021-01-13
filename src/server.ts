f8f3ae3979af9985cfaba127f49d7e4031150ff0
Leonardo Theodoro <simplespinheiro@gmail.com>
2021-01-13 10:29:59 -0300

    creating appointments

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,7 @@ import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server started on port 3333");
